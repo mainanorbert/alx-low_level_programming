1cdb28f72c96fccab8dc7cb615de8409ee879873
mainanorbert <mainanorbert@gmail.com>
2023-03-16 18:29:56 +0000

    my first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(void){
+
+/**
+ * Program starts at main
+ *
+ *returns a zero for success
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
