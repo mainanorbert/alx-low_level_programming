07c3dbf7f69fe380c0f52f23e1e9696059ef9a33
mainanorbert <mainanorbert@gmail.com>
2023-03-16 21:22:52 +0000

    size of data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - indicates the start of a program
+ *
+ * Return: The program returns a 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+	long int d;
+	long long int c;
+	float e;
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return(0);
+}
