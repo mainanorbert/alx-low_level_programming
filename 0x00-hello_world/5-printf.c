19a28bcc66251e034b0d9df970a31de8497dfd3b
mainanorbert <mainanorbert@gmail.com>
2023-03-16 20:01:34 +0000

    prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-entry point of the program
+ *
+ * Return: returns void 0
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+	}
