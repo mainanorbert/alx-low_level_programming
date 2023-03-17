cfec068f7e64c627a66399952fdf1e6ef1f76660
mainanorbert <mainanorbert@gmail.com>
2023-03-17 07:09:31 +0000

    size of characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 
 /**
  *
- * main - indicates the start of a program
+ * main - The program starts at main function
  *
- * Return: The program returns a 0
+ * Return: the program returns 0
  */
 int main(void)
 {
@@ -13,10 +13,11 @@ int main(void)
 	long int d;
 	long long int c;
 	float e;
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-	return(0);
+	return (0);
 }
