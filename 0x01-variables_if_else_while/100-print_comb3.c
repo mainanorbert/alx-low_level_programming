492c200d451da25a316adc99c35adc2124683445
mainanorbert <mainanorbert@gmail.com>
2023-03-19 11:08:41 +0000

    print all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(j);
+			putchar(i);
+			if (i == '9' && j == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	putchar('\n');
+	return (0);
+}
