f37d71929bbdcd6d5d9be4972d505ad3b62aade4
mainanorbert <mainanorbert@gmail.com>
2023-03-20 14:16:36 +0000

    printing using write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point of the program
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	61);
+	return (1);
 }
 
