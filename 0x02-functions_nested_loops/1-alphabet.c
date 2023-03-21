e6b42449ee998263ea49de2312fbb5e5d685a414
mainanorbert <mainanorbert@gmail.com>
2023-03-21 12:47:25 +0000

    printing using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h" 
+
+/**
+ * print_alphabet - the program starts hear
+ *
+ * Return: the program retuns void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
