b906a6b1ae9ccc2316ad2de909fd5d402e9e524b
mainanorbert <mainanorbert@gmail.com>
2023-03-21 08:03:09 +0000

    printing with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - where the program starts
+ *
+ * Return: The program returns 0
+ */
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directiry contain C source files for functions and nested loops
