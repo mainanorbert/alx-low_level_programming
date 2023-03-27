528fb0f049299d33c31de053cf191a145a8eda91
mainanorbert <mainanorbert@gmail.com>
2023-03-27 21:08:48 +0000

    printing integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints array of n elements
+ * @a: array
+ * @n: no of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+}
