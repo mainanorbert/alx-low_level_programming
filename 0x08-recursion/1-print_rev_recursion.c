d7afda9f2c914f6fb6abb7a8b565fc275338d2a3
mainanorbert <mainanorbert@gmail.com>
2023-04-05 13:55:41 +0000

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to a string
+ */
+void _print_rev_recursion(char *s)
+{
+	/*t len = strlen(s);*/
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
