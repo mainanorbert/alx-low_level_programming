20dd9a20e806ad672b4ec54bc2daa3546b8c433c
mainanorbert <mainanorbert@gmail.com>
2023-04-10 14:33:02 +0000

    adding integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive integers
+ * @argc: number of arguments
+ * @argv: array of pointers containing strings
+ * Return: returns an int
+ */
+int main(int argc, char *argv[])
+{
+	int i, j = 0, sum;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		while (argv[i][j])
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error");
+				return (1);
+			}
+			j++;
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
