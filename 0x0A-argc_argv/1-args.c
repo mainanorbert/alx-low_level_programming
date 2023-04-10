b61eeb28ed0305a8fbbe1922b4730d4d2d4f7af8
mainanorbert <mainanorbert@gmail.com>
2023-04-10 05:29:14 +0000

    printing number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of pointers to strings (arguments)
+ * Return: returns 0 for success
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
