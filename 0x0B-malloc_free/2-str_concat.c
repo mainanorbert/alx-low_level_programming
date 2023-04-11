95d8c3fc51c5e2a847b0711dc29b715fc06628f6
mainanorbert <mainanorbert@gmail.com>
2023-04-11 14:59:42 +0000

    string that copies two strings to memory location

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: pointer to a string
+ * @s2: pointer to string 2
+ * Return: pointer to newly alloacted string with the 2 strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1 = strlen(s1), len2 = strlen(s2);
+	char *mem_al;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	mem_al = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+	if (mem_al == NULL)
+	{
+		return (NULL);
+	}
+	memcpy(mem_al, s1, len1);
+	memcpy(mem_al, s2, len2 + 1);
+	return (mem_al);
+}
