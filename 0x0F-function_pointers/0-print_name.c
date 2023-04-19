49e4f65245af7de7dd92468d8604544c4c397959
mainanorbert <mainanorbert@gmail.com>
2023-04-19 13:38:41 +0000

    function that prints a nane

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_name - function that prints name
+ * @name: pointer to string
+ * @f: pointer to function with pointer character
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
