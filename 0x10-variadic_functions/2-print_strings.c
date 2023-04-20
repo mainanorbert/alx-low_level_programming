697e247dc12abb48b1e290befd7e898437f87198
mainanorbert <mainanorbert@gmail.com>
2023-04-20 11:31:10 +0000

    prints a string of variadic function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints strings
+ * @separator: pointer to character
+ * @n: number of strings passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list strs;
+
+	va_start(strs, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strs, char*);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		printf("%s", str);
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strs);
+}
