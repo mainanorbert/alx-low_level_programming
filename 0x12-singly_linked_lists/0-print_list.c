1ced4e7a427d0fa0dc85b75b35367742c7688ce7
mainanorbert <mainanorbert@gmail.com>
2023-04-27 19:26:12 +0000

    correction of length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t c = 0;
-	/*list_t myhead =  h;*/
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
-		printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
-		c++;
+		count++;
 	}
-	return (c);
+	return (count);
 }
