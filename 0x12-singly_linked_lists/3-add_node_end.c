0ad09790706d60fece16cb572237c42cb408762d
mainanorbert <mainanorbert@gmail.com>
2023-04-27 21:37:00 +0000

    adding element at end of list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,21 +9,21 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_n;
 	list_t *last_n = *head;
-	unsigned int len = 0;
+	unsigned int i = 0;
 
-	while (str[len])
-		len++;
+	while (str[i])
+		i++;
 	new_n = malloc(sizeof(list_t));
 	if (new_n == NULL)
 		return (NULL);
 
 	new_n->str = strdup(str);
-	new_n->len = len;
+	new_n->len = i;
 	new_n->next = NULL;
-	if (*head == NULL)
+	if (last_n == NULL)
 	{
 		*head = new_n;
-		return (NULL);
+		return (new_n);
 	}
 	while (last_n->next)
 	{
