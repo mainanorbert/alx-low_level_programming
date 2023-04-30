15b4963444f726070e546a93822927fe0186a2e2
mainanorbert <mainanorbert@gmail.com>
2023-04-30 21:09:24 +0000

    freeing sll and setting head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees lists and sets head to null
+ * @head: first node in the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_curr = *head;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	while (free_curr)
+	{
+		temp = free_curr;
+		free_curr = free_curr->next;
+		free(temp);
+	}
+	*head = NULL;
+}
