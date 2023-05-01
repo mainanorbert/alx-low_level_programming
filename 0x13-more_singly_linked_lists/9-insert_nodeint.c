24c06b8ce048322f6eba0767b592b06f6b1477b4
mainanorbert <mainanorbert@gmail.com>
2023-05-01 20:30:05 +0000

    inserts node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (head == NULL)
+	if (i == 0)
 	{
 		new->next = *head;
 		*head = new;
