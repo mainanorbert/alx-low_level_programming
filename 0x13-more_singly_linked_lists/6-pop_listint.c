e2bdc9adec9ba1c5a3927431ed5efda97b85a790
mainanorbert <mainanorbert@gmail.com>
2023-05-01 07:25:53 +0000

    deletes head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,14 +2,14 @@
 /**
  * pop_listint - deletes head node and returns its data(n)
  * @head: first node in list
- * Return: 0 if empty and one if deleted
+ * Return: 0 if empty and head node's data (n)
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp_n;
 	int n;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return (0);
 	n = (*head)->n;
 	temp_n = (*head)->next;
