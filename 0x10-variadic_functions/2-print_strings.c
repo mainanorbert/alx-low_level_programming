81dda7e9831fbf5678ad7c0bdfeeb628361abe22
mainanorbert <mainanorbert@gmail.com>
2023-05-02 11:15:02 +0000

    prints strings git push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,9 +21,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 		printf("%s", str);
-		if (separator != NULL && i < n - 1)
-			printf("%s", separator);
+		printf(separator && i < n - 1 ? "%s" : "", separator);
+		/*if (separator != NULL && i < n - 1)*/
+			/*printf("%s", separator);*/
 	}
-	printf("\n");
 	va_end(strs);
+	printf("\n");
 }
