145c45a68bd97996594172da1149ca4f3c8108ab
mainanorbert <mainanorbert@gmail.com>
2023-05-03 21:00:31 +0000

    reversing a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,16 +7,15 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *next_n;
+	listint_t *next_n = NULL;
 	listint_t *prev_n = NULL;
-	listint_t *curr_n = *head;
 
-	while (curr_n)
+	while (*head)
 	{
-		next_n = curr_n->next;
-		curr_n->next = prev_n;
-		prev_n = curr_n;
-		curr_n = next_n;
+		next_n = (*head)->next;
+		(*head)->next = prev_n;
+		prev_n = *head;
+		*head = next_n;
 	}
 
 	*head = prev_n;
