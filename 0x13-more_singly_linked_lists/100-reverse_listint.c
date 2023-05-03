5646737ebd6aae8f21810f8aa2a08c8669f57967
mainanorbert <mainanorbert@gmail.com>
2023-05-03 07:29:45 +0000

    reverses a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,4 +7,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *next_n;
+	listint_t *prev_n = NULL;
+	listint_t *curr_n = *head;
 
+	while (curr_n != NULL)
+	{
+		next_n = curr_n->next;
+		curr_n->next = prev_n;
+		prev_n = curr_n;
+		curr_n = next_n;
+	}
+	*head = prev_n;
+	return (*head);
+}
