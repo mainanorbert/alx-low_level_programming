b7b58d28bb9531b1f30c1d7845d07114209651d4
mainanorbert <mainanorbert@gmail.com>
2023-05-04 15:41:05 +0000

    binary to decimal

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints single character
+ * @c: character to be printed
+ * Return: returns integer
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
