ee4d7a60a6c483735d4f9cf2458020ed0aaee196
mainanorbert <mainanorbert@gmail.com>
2023-05-09 05:52:11 +0000

    copying code from one file to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - entry point of program
  * @argc: no. of arguments
@@ -11,18 +10,17 @@ int main(int argc, char *argv[])
 	int from_file, to_file;
 	ssize_t bytes_r, bytes_w;
 	char buffer[BUFFER_SIZE];
+	char arg[20];
+	char arg2[20];
 
 	if (argc != 3)
 	{
-		print_error("Usage: %s file_from file_to", argv[0]);
+		print_error("Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	from_file = open(argv[1], O_RDONLY);
 	if (from_file == -1)
-	{
-		print_error("Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		print_error("Error: Can't read from file %s\n", argv[1]), exit(98);
 	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to_file == -1)
 	{
@@ -33,24 +31,18 @@ int main(int argc, char *argv[])
 	{
 		bytes_w = write(to_file, buffer, bytes_r);
 		if (bytes_w == -1)
-		{
-			print_error("Error: Can't write to file %s\n", argv[2]);
-			exit(99);
-		}
+			print_error("Error: Can't write to file %s\n", argv[2]), exit(99);
 	}
 	if (bytes_r == -1)
-	{
-		print_error("Error: Can't read from %s\n", argv[1]);
-		exit(98);
-	}
+		print_error("Error: Can't read from %s\n", argv[1]), exit(98);
 	if (close(from_file) == -1)
 	{
-		print_error("Error: Can't close fd %d", from_file);
+		sprintf(arg, "%d", from_file), print_error("Error: Can't close fd %s", arg);
 		exit(100);
 	}
 	if (close(to_file) == -1)
 	{
-		print_error("Error: Can't close fd %d", to_file);
+		sprintf(arg2, "%d", to_file), print_error("Error: Can't close fd %s", arg2);
 		exit(100);
 	}
 	return (0);
