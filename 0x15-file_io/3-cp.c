b3ca43027779e9a976fe700817f3f95c697f639b
mainanorbert <mainanorbert@gmail.com>
2023-05-09 18:21:41 +0000

    copying from one file to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	int file_from, file_to, x, y;
 	ssize_t bytes_r, bytes_w;
 	char buffer[BUFFER_SIZE];
 
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	}
 	while ((bytes_r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_w = write(file_to, buffer, BUFFER_SIZE);
+		bytes_w = write(file_to, buffer, bytes_r);
 		if (bytes_w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
@@ -39,9 +39,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(file_from) == -1)
+	x = close(file_from), y = close(file_to);
+	if (x == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from), exit(100);
-	if (close(file_to) == -1)
+	if (y == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to), exit(100);
 	}
