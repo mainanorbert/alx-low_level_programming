3827e6932262a62d2b7d78e4467bf92ece8f82e7
mainanorbert <mainanorbert@gmail.com>
2023-05-09 06:14:38 +0000

    copying files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,39 +10,40 @@ int main(int argc, char *argv[])
 	int from_file, to_file;
 	ssize_t bytes_r, bytes_w;
 	char buffer[BUFFER_SIZE];
-	char arg[20];
-	char arg2[20];
 
 	if (argc != 3)
 	{
-		print_error("Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	from_file = open(argv[1], O_RDONLY);
 	if (from_file == -1)
-		print_error("Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+	}
 	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to_file == -1)
 	{
-		print_error("Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 	while ((bytes_r = read(from_file, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_w = write(to_file, buffer, bytes_r);
 		if (bytes_w == -1)
-			print_error("Error: Can't write to file %s\n", argv[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	if (bytes_r == -1)
-		print_error("Error: Can't read from %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 	if (close(from_file) == -1)
-	{
-		sprintf(arg, "%d", from_file), print_error("Error: Can't close fd %s", arg);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", from_file), exit(100);
 	if (close(to_file) == -1)
 	{
-		sprintf(arg2, "%d", to_file), print_error("Error: Can't close fd %s", arg2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", to_file);
 		exit(100);
 	}
 	return (0);
