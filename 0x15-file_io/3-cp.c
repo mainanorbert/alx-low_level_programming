2bea24978bf309104d10dca6baea41dc094b3ff4
mainanorbert <mainanorbert@gmail.com>
2023-05-09 06:37:03 +0000

    copy between two files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int from_file, to_file;
+	int file_from, file_to;
 	ssize_t bytes_r, bytes_w;
 	char buffer[BUFFER_SIZE];
 
@@ -16,21 +16,21 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	from_file = open(argv[1], O_RDONLY);
-	if (from_file == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 	}
-	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to_file == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	while ((bytes_r = read(from_file, buffer, BUFFER_SIZE)) > 0)
+	while ((bytes_r = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_w = write(to_file, buffer, bytes_r);
+		bytes_w = write(file_to, buffer, bytes_r);
 		if (bytes_w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
@@ -39,11 +39,11 @@ int main(int argc, char *argv[])
 	}
 	if (bytes_r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
-	if (close(from_file) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", from_file), exit(100);
-	if (close(to_file) == -1)
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from), exit(100);
+	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", to_file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
 		exit(100);
 	}
 	return (0);
