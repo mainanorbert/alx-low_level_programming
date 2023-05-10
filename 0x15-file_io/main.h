1f6c54dc9f47faacb705a4a1a822531a730e62ca
mainanorbert <mainanorbert@gmail.com>
2023-05-10 19:20:13 +0000

    Display information contained in the ELF

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <elf.h>
 
 #define BUFFER_SIZE 1024
 
@@ -12,5 +13,6 @@ int  _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_elf_h(const Elf64_Ehdr* h);
 /*void print_error(const char *message, const char *arg);*/
 #endif
