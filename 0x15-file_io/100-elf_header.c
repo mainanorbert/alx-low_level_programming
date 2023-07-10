d48717f9e18ca3a94e54d7313226082d88019dae
mainanorbert <mainanorbert@gmail.com>
2023-07-10 20:11:46 +0000

    elf headers

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,6 +5,32 @@
 #include <string.h>
 #include <elf.h>
 #include "main.h"
+
+/**
+ * error_func - prints error message*
+ * @message: error message
+ */
+void error_func(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
+/**
+ * magic_functions - prints magic values
+ * @h: elf header
+ */
+void magic_functions(const Elf64_Ehdr *h)
+{
+	int x;
+
+	printf("ELF Header:\n");
+	printf("Magic:");
+	for (x = 0; x < EI_NIDENT; ++x)
+	{
+		printf("%02x", h->e_ident[x]);
+	}
+	printf("\n");
+}
 /**
  * print_class_code - class code
  * @h: elf header
