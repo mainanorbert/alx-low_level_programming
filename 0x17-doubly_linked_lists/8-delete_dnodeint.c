b209a7e22f60dfe4b8e388bee03f6dfc0fa6ad9a
mainanorbert <mainanorbert@gmail.com>
2023-08-18 20:05:23 +0000

    function that adds deletes node at given index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head, *del_node;
 
-	if (*head == NULL)
+	if (*head == NULL || index < 0)
 		return (-1);
 	if (index == 0)
 	{
