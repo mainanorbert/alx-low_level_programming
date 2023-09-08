8e0dba8e35e7a4c7f73c6d976ece43762b220ac9
mainanorbert <mainanorbert@gmail.com>
2023-09-08 11:36:06 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 0-hash_table_create.c 1-djb2.c 2-key_index.c 3-hash_table_set.c -o d

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,5 +39,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = value_cpy;
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
+	free(value_cpy);
 	return (1);
 }
