0dcaa870cd15b75c7ff7142da3d0273505549c42
mainanorbert <mainanorbert@gmail.com>
2023-09-08 11:38:43 +0000

    function that adds element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,6 +39,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = value_cpy;
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
-	free(value_cpy);
 	return (1);
 }
