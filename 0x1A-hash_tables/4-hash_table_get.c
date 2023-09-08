4c04beab65d64a9ecf4def9d3f2f9955bdd50553
mainanorbert <mainanorbert@gmail.com>
2023-09-08 09:41:21 +0000

    retrieving value of key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with key
+ * @ht: pointer to hash table with value/key array
+ * @key: key whose value is retrieved
+ * Return: pointer to value associated with key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *my_node;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	my_node = ht->array[idx];
+
+	while (my_node)
+	{
+		if (strcmp(my_node->key, key) == 0)
+			return (my_node->value);
+		my_node = my_node->next;
+	}
+	return (NULL);
+}
