d48a8559dc02d58612b50a8796131f0b1695c859
mainanorbert <mainanorbert@gmail.com>
2023-09-08 09:05:20 +0000

    adds element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
+		free(value_cpy);
 		free(new_node);
 		return (0);
 	}
