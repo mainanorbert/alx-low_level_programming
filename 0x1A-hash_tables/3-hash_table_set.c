ccdf27c679d6fc0624a650e0af66936cd263ab25
mainanorbert <mainanorbert@gmail.com>
2023-09-10 18:18:36 +0000

    correction on indexing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,38 +6,44 @@
  * @value: value to be stored
  * Return: 1 for success and 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	unsigned long int idx, i;
-	char *value_cpy = NULL, *key_cpy = NULL;
+	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-	value_cpy = strdup(value);
-	if (value_cpy == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
-	i = idx;
 	while (ht->array[idx])
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(ht->array[idx]->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_cpy;
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = strdup(value);
+			if (ht->array[idx]->value == NULL)
+				return (0);
 			return (1);
 		}
-		i++;
+		idx++;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		free(value_cpy);
 		return (0);
 	}
-	key_cpy = strdup(key);
-	new_node->key = key_cpy;
-	new_node->value = value_cpy;
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key), free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
 	return (1);
